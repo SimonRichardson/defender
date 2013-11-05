c041532fe8f38a229968813e5c71e4e713d82b8c
Simon Richardson <stickupkid@gmail.com>
2013-11-05 10:03:11 +0000

    Initial commit.

---
+++ defenders.js
@@ -0,0 +1,13 @@
+var defender = require('./src/defender'),
+    fortune = require('./src/fortune'),
+    guardian = require('./src/guardian'),
+    soothsayer = require('./src/soothsayer'),
+    runes = require('./src/runes');
+
+exports = module.exports = {
+    defender: defender,
+    fortune: fortune,
+    guardian: guardian,
+    soothsayer: soothsayer,
+    runes: runes
+};
