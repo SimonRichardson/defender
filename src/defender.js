e2d1ebd6777e6af9dec40c3f764115f86e4252a4
Simon Richardson <stickupkid@gmail.com>
2013-12-09 18:02:27 +0000

    Slowly getting somewhere!
    - Wiring up the chaining of events.
    
    - Free field example is working again.
    
    - Restricted example is working again!
    
    - Removing note

--- src/defender.js
+++ src/defender.js
@@ -1,22 +1,25 @@
-var Validation = require('fantasy-validations'),
-    combinators = require('fantasy-combinators'),
+var combinators = require('fantasy-combinators'),
+    daggy = require('daggy'),
+    
     IO = require('fantasy-io'),
-    Maybe = require('fantasy-options'),
     State = require('fantasy-states'),
     Tuples = require('fantasy-tuples'),
-    Either = require('fantasy-eithers'),
 
     compose = combinators.compose,
     constant = combinators.constant,
 
-    Success = Validation.Success,
-    Failure = Validation.Failure,
-
-    Left = Either.Left,
-    Right = Either.Right,
-
     Tuple2 = Tuples.Tuple2,
-    Tuple3 = Tuples.Tuple3,
+    Tuple4 = Tuples.Tuple4,
+
+    Triad = daggy.taggedSum({
+        Success: ['x'],
+        Maybe: ['flow'],
+        Failure: ['errors']
+    }),
+    Flow = daggy.taggedSum({
+        Underflow: ['underflow', 'position'],
+        Overflow: ['overflow', 'position']
+    }),
 
     executeExpr = function(a) {
         return function(b) {
@@ -24,91 +27,132 @@ var Validation = require('fantasy-validations'),
         };
     },
 
-    validateString = function(string) {
-        if(possible) {
-            string = string.slice(possible[0].length);
-        } else {
-            position = (a.length - string.length) + 1;
-            accum.push(Tuple3(string, Maybe.Some(value), position));
-            /* Let's move the string onwards */
-            if (string.length < 1)
-                return Tuple2(string, accum);
-            string = string.slice(1);
-        }
+    push = function(a, b) {
+        var x = a.slice();
+        x.push(b);
+        return x;
+    },
+
+    validateString = function(program, index, accum, original, modified) {
+        return function(possible) {
+            if(possible) {
+                return constant(
+                    Tuple4(
+                        program,
+                        original,
+                        modified.slice(possible[0].length),
+                        accum
+                    )
+                );
+            } else {
+                return constant(
+                    Tuple4(
+                        program,
+                        original,
+                        modified,
+                        push(accum, Tuple2(modified, index))
+                    )
+                );
+            }
+        };
     },
 
     consume = function(a) {
-        var M = State.StateT(IO);
+        var M = State.StateT(IO),
 
-        return function(b) {
-            var rec = function(index, program) {
-                return function(string) {
-                    if (index < b.length) {
+            rec = function(index, guards) {
+                return function(tuple) {
+                    var program = tuple._1,
+                        original = tuple._2,
+                        modified = original.length >= 1 ? original.slice(index) : original,
+                        maybes = tuple._4,
+                        guard;
+
+                    if (index < guards.length) {
+                        guard = guards[index];
+
                         return program
-                                .chain(constant(M.lift(b[index])))
-                                .chain(constant(M.modify)(executeExpr(string)))
-                                .chain(constant(M.get))
-                                .chain(constant(M.modify)(validateString))
-                                .chain(constant(M.get))
-                                .chain(constant(M.modify)(rec(index + 1, program)));
+                            .chain(constant(M.lift(guard)))
+                            .chain(compose(M.modify)(executeExpr(modified)))
+                            .chain(constant(M.get))
+                            .chain(compose(M.modify)(validateString(program, index, maybes, original, modified)))
+                            .chain(constant(M.get))
+                            .chain(rec(index + 1, guards));
                     } else {
                         return program;
                     }
                 };
             };
-            result = rec(0, M.of(''))(a);
-            console.log(result.exec('').unsafePerform());
-
-            return Tuple3(a, '', '');
+        return function(b) {
+            return rec(0, b)(Tuple4(M.of(''), a, '', []));
         };
     },
 
-    containsError = function(x) {
+    contains = function(x, f) {
         var i;
-        for(i = 0; i < x.length; i++) {
-            if (x[i]._1 !== '')
+        for (i = 0; i < x.length; i++) {
+            if (f(x[i]))
                 return true;
         }
         return false;
     },
 
-    createFailure = function(string, position) {
-        return Failure([Tuple3(string, Maybe.None, position)]);
+    filter = function(x, f) {
+        var accum = [],
+            i;
+        for (i = 0; i < x.length; i++) {
+            if (f(x[i]))
+                accum.push(x[i]);
+        }
+        return accum;
     },
 
-    output = function() {
-        return function(x) {
-            var string = x._2,
-                possibleErrors = x._3;
+    empty = function(x) {
+        return x._1 !== '';
+    },
 
-            /* Re-factor this, as it's really messy */
-            if (possibleErrors.length < 1 && string.length > 0) {
-                /* Check for overflows */
-                return Left(createFailure(string, (x._1.length - string.length) + 1));
+    output = function(a) {
+        return function() {
+            var original = a._2,
+                modified = a._3,
+                possibleErrors = a._4,
+                actualErrors = filter(possibleErrors, empty);
+
+            if (possibleErrors.length < 1 && modified.length > 0) {
+                // Check for overflows
+                return Triad.Maybe(Flow.Overflow(modified, original.length - modified.length));
 
             } else if(possibleErrors.length > 0) {
                 
-                if (string.length < 1 && !containsError(possibleErrors)) {
-                    /* Check for underflows */
-                    return Right(createFailure(string, string.length));
+                if (modified.length < 1 && !contains(possibleErrors, empty)) {
+                    // Check for underflows
+                    return Triad.Maybe(Flow.Underflow(modified, modified.length));
 
                 } else
-                    return Left(Failure(possibleErrors));
+                    return Triad.Failure(actualErrors);
 
             } else
-                return Right(Success(x._1));
+                return Triad.Success(original);
         };
     },
 
     defender = function(sayings) {
         return function(stream) {
             var M = State.StateT(IO),
+
                 program =
                     M.lift(sayings)
                     .chain(compose(M.modify)(constant))
                     .chain(constant(M.lift(stream)))
-                    .chain(compose(M.modify)(consume))
+                    .chain(function(a) {
+                        return M.modify(function(b) {
+                            return consume(a)(b).exec('');
+                        });
+                    })
                     .chain(constant(M.get))
+                    .chain(function(a) {
+                        return M.lift(a);
+                    })
                     .chain(compose(M.modify)(output))
                     .chain(constant(M.get));
                     
